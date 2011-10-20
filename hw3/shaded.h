e7b4e594d7b7075257a6122d36e2e7fb0a720b56
Zack Gomez <zacharyg@caltech.edu>
2011-10-20 00:50:48 -0700

    Added hw3 directory from hw2 directory.
    Also added some new structure in the shaded.h and some new tokens in
    shaded.lex.

---
+++ hw3/shaded.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "matrix.h"
+#include <vector>
+
+struct Camera
+{
+    Vector3 position;
+    Vector4 orientation;
+    float nearDistance;
+    float farDistance;
+    float left;
+    float right;
+    float top;
+    float bottom;
+};
+
+struct Light
+{
+    Vector3 position;
+    Vector3 color;
+};
+
+struct Material
+{
+    Vector3 ambientColor;
+    Vector3 diffuseColor;
+    Vector3 specularColor;
+    float shininess;
+};
+
+struct Separator
+{
+    Matrix4 transform;
+    std::vector<Vector3> points;
+    std::vector<int> indices;
+    std::vector<Vector3> normals;
+    std::vector<int> normalindices;
+
+    Material material;
+};
+
+struct Scene
+{
+    Camera camera;
+    std::vector<Separator> separators;
+    std::vector<Light> lights;
+};
